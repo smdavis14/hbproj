b862e3fb1af86e95e4de504d9d6e8788299117f5
smdavis14 <smdavis14@yahoo.com>
2021-08-28 06:56:13 -0700

    shopping cart added

--- HillBakes/HillBakes/settings.py
+++ HillBakes/HillBakes/settings.py
@@ -66,10 +66,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
 ]
+
+CART_SESSION_ID = 'cart'
 
 WSGI_APPLICATION = 'HillBakes.wsgi.application'
 
--- HillBakes/cart/views.py
+++ HillBakes/cart/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hb.models import Product, Cost
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
 
 # Create your views here.
-def shopping_cart(request):
-    print(request.POST)
-    return render(request, 'hb/shopping_cart.html')
+#@login_required(login_url="/users/login")
+def cart_add(request, productID, costID):
+    cart = Cart(request)
+    product = Product.objects.get(id=productID)
+    cost = Cost.objects.get(id=costID)
+    cart.add(product=product, cost=cost)
+    return redirect("hb:home_page")
+
+
+#@login_required(login_url="/users/login")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("hb:home_page")
+
+
+#@login_required(login_url="/users/login")
+def item_increment(request, productID, costID):
+    cart = Cart(request)
+    product = Product.objects.get(id=productID)
+    cost = Cost.objects.get(id=costID)
+    cart.add(product=product, cost=cost)
+    return redirect("hb:home_page")
+
+
+#@login_required(login_url="/users/login")
+def item_decrement(request, productID, costID):
+    cart = Cart(request)
+    product = Product.objects.get(id=productID)
+    cost = Cost.objects.get(id=costID)
+    cart.decrement(product=product)
+    return redirect("hb:home_page")
+
+
+#@login_required(login_url="/users/login")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("hb:home_page")
+
+
+#@login_required(login_url="/users/login")
+def cart_detail(request):
+    cart = Cart(request)
+    context = {
+        'cart': cart
+    }
+    return render(request, 'cart/cart_detail.html', context)
