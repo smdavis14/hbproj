4ab3665fe90683ca0590054ea178ebe20ccdeac6
smdavis14 <smdavis14@yahoo.com>
2021-08-30 17:39:43 -0700

    styling updates

--- HillBakes/hb/urls.py
+++ HillBakes/hb/urls.py
@@ -1,6 +1,8 @@
 from django.apps import apps
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home_page, cookies, cakes, cake_balls, about
 
 app_name = 'hb'
@@ -10,4 +12,4 @@ urlpatterns = [
     path('cakes/', cakes, name='cakes'),
     path('cake_balls/', cake_balls, name='cake_balls'),
     path('about/', about, name='about'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
